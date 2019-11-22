13117c583f4619d74005227d89645b09bc0611cd
EnoshTs <enosh.tsur@haaretz.co.il>
2019-11-22 19:38:06 +0200

    something

---
+++ arrayFunc1.js
@@ -0,0 +1,21 @@
+
+function add(prev, next) { return prev + next }
+function isNotInRange(array, i) { return i <= 0 || i >= array.length }
+function arrayTooShort(array) { return array.length <= 1 }
+
+function isBalanced(index) {
+    return array => {
+        if (isNotInRange(array, index) || arrayTooShort(array)) {
+            return false;
+        }
+        const halfOne = array.slice(0, index);
+        const halfTwo = array.slice(index, array.length);
+        return halfOne.reduce(add) === halfTwo.reduce(add);
+    }
+}
+
+const isBalanced4 = isBalanced(4);
+console.log(isBalanced4([1, 2, 2, 1, 3, 2, 1, 0]))
+
+const isBalanced2 = isBalanced(2);
+console.log(isBalanced2([1, 2, 0, 3]))
---
+++ arrayFunc2.js
@@ -0,0 +1,17 @@
+function add(prev, next) { return prev + next }
+function isNotInRange(array, index) { return index <= 0 || index >= array.length }
+function arrayTooShort(array) { return array.length <= 1 }
+
+function isBalanced(array, index) {
+    if (isNotInRange(array, index) || arrayTooShort(array)) {
+        return false;
+    }
+    const halfOne = array.slice(0, index);
+    const halfTwo = array.slice(index, array.length);
+    return halfOne.reduce(add) === halfTwo.reduce(add);
+}
+
+
+console.log(isBalanced([1, 2, 2, 1, 3, 2, 1, 0], 4))
+
+console.log(isBalanced([1, 2, 0, 3], 2))
